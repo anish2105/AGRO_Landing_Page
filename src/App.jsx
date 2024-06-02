50294b618866016675a5e34654343ef2be87bafc
anish2105 <anishvv212002@gmail.com>
2024-06-02 12:34:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Logo_cloud from './components/Logo_cloud'
+import Navbar from './components/Navbar'
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Logo_cloud/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
